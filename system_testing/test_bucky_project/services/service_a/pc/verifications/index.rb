fb6c7c23d78e94a4b49be1c9cc35ea2dbc0a7148
NA070 <naotospace@gmail.com>
2019-07-03 22:33:40 +0900

    add system tesing for find multiple elements

---
+++ system_testing/test_bucky_project/services/service_a/pc/verifications/index.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'bucky/test_equipment/verifications/e2e_verification'
+module Services
+  module ServiceA
+    module Pc
+      module Verifications
+        class Index < Bucky::TestEquipment::Verifications::E2eVerification
+          def click_single_element(_)
+            @pages.index.links.click
+          end
+
+          def click_multiple_element(_)
+            @pages.index.links[1].click
+          end
+        end
+      end
+    end
+  end
+end
