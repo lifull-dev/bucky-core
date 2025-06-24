e1c14829cf112fe74afcce06107ee793ae195a1f
shouan202212 <shouan@lifull.com>
2025-06-24 17:13:17 +0900

    add_end_time_jobs

--- lib/bucky/core/database/test_data_operator.rb
+++ lib/bucky/core/database/test_data_operator.rb
@@ -15,13 +15,15 @@ module Bucky
 
         # Save job data and return job id
         # @param  [Time] start_time
+        # @param  [Time] end_time
+        # @param  [Time] duration
         # @param  [String] command_and_option
         # @param  [String] fqdn
         # @return [Fixnum] job_id
-        def save_job_record_and_get_job_id(start_time, command_and_option, fqdn)
+        def save_job_record_and_get_job_id(start_time, end_time, duration, command_and_option, fqdn)
           return 0 if $debug
 
-          job_id = @connector.con[:jobs].insert(start_time: start_time, command_and_option: command_and_option, base_fqdn: fqdn)
+          job_id = @connector.con[:jobs].insert(start_time: start_time, end_time: end_time, exe_duration: duration, command_and_option: command_and_option, base_fqdn: fqdn)
           @connector.disconnect
           job_id
         end
